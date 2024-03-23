e550e2bb02ffd6c670b80a3e075bcb5d02ebf044
reza drakhshan <github2reza@gmail.com>
2024-03-23 13:53:49 +0330

    set state and city

--- MeteorologyBot/main.py
+++ MeteorologyBot/main.py
@@ -2,10 +2,6 @@ from telethon import TelegramClient, events, Button
 from config import *
 from messages import start_message
 from telethon.tl.custom.message import Message
-from city.Tehran.messages import msg as tehran
-from city.Karaj.messages import msg as karaj
-from city.Esfahan.messages import msg as esfahan
-from city.Shiraz.messages import msg as shiraz
 
 client = TelegramClient("bot_session", api_id=api_id, api_hash=api_hash)
 
@@ -13,120 +9,419 @@ client = TelegramClient("bot_session", api_id=api_id, api_hash=api_hash)
 @client.on(events.NewMessage(pattern=r"/start"))
 async def start(event):
     text = start_message()
-    markup = client.build_reply_markup(
-        [
-            [
-                Button.inline("دما", data="temperature"),
-                Button.inline("راهنما", data="help"),
-            ],
-            [
-                Button.inline("درباره ما", data="about"),
-                Button.inline("حمایت از ما", data="donate"),
-            ],
-        ]
-    )
     text: Message = await client.send_message(
         entity=event.chat_id,
         message=text,
         parse_mode="html",
-        buttons=markup,
+    )
+    markup = client.build_reply_markup(
+        [
+            [
+                Button.inline("تهران", data="Tehran"),
+                Button.inline("کهگیلویه و بویراحمد", data="KohgiluyehandBoyerAhmad"),
+                Button.inline("آذربایجان شرقی", data="EastAzerbaijan"),
+            ],
+            [
+                Button.inline("آذربایجان غربی", data="WestAzerbaijan"),
+                Button.inline("اردبیل", data="Ardabil"),
+                Button.inline("اصفهان", data="Isfahan"),
+            ],
+            [
+                Button.inline("البرز", data="Alborz"),
+                Button.inline("ایلام", data="Ilam"),
+                Button.inline("بوشهر", data="Bushehr"),
+            ],
+            [
+                Button.inline("چهارمحال و بختیاری", data="ChaharmahalandBakhtiari"),
+                Button.inline("خراسان جنوبی", data="SouthKhorasan"),
+                Button.inline("خراسان رضوی", data="RazaviKhorasan"),
+            ],
+            [
+                Button.inline("خراسان شمالی", data="NorthKhorasan"),
+                Button.inline("خوزستان", data="Khuzestan"),
+                Button.inline("زنجان", data="Zanjan"),
+            ],
+            [
+                Button.inline("سمنان", data="Semnan"),
+                Button.inline("سیستان و بلوچستان", data="SistanandBaluchestan"),
+                Button.inline("فارس", data="Fars"),
+            ],
+            [
+                Button.inline("قزوین", data="Qazvin"),
+                Button.inline("قم", data="Qom"),
+                Button.inline("کردستان", data="Kurdistan"),
+            ],
+            [
+                Button.inline("کرمان", data="Kerman"),
+                Button.inline("کرمانشاه", data="Kermanshah"),
+                Button.inline("گلستان", data="Golestan"),
+            ],
+            [
+                Button.inline("گیلان", data="Gilan"),
+                Button.inline("لرستان", data="Lorestan"),
+                Button.inline("مازندران", data="Mazandaran"),
+            ],
+            [
+                Button.inline("مرکزی", data="Markazi"),
+                Button.inline("هرمزگان", data="Hormozgan"),
+                Button.inline("همدان", data="Hamadan"),
+            ],
+            [
+                Button.inline("یزد", data="Yazd"),
+            ],
+        ]
+    )
+    await client.send_message(
+        entity=event.chat_id, message="استان خود را وارد کنید:", buttons=markup
     )
 
 
 @client.on(events.CallbackQuery)
-async def callback_query(event):
-    if event.data == b"help":
-        await client.send_message(entity=event.chat_id, message="راهنما")
-    if event.data == b"temperature":
-        markup = client.build_reply_markup(
-            [
-                [
-                    Button.inline("تهران", data="Tehran"),
-                    Button.inline("اصفهان", data="Esfahan"),
-                    Button.inline("شیراز", data="Shiraz"),
-                ],
-                [
-                    Button.inline("مشهد", data="Mashhad"),
-                    Button.inline("تبریز", data="Tabriz"),
-                    Button.inline("اهواز", data="Ahvaz"),
-                ],
-                [
-                    Button.inline("کرمانشاه", data="Kermanshah"),
-                    Button.inline("قم", data="Qom"),
-                    Button.inline("کرج", data="Karaj"),
-                ],
-                [
-                    Button.inline("اراک", data="Arak"),
-                    Button.inline("قزوین", data="Qazvin"),
-                    Button.inline("سنندج", data="Sanandaj"),
-                ],
-                [
-                    Button.inline("زاهدان", data="Zahedan"),
-                    Button.inline("همدان", data="Hamedan"),
-                    Button.inline("ارومیه", data="Urmia"),
-                ],
-                [
-                    Button.inline("رشت", data="Rasht"),
-                    Button.inline("یزد", data="Yazd"),
-                    Button.inline("اردبیل", data="Ardabil "),
-                ],
-                [
-                    Button.inline("کرمان", data="Kerman"),
-                    Button.inline("بندرعباس", data="BandarAbbas"),
-                ],
-                [
-                    Button.inline("بازگشت", data="back"),
-                ],
-            ]
-        )
-        await client.edit_message(
-            event.chat_id,
-            event.message_id,
-            text="شهر مورد نظر را انتخاب کنید :",
-            buttons=markup,
-        )
-    if event.data == b"about":
-        await client.send_message(entity=event.chat_id, message="درباره ربات")
-    if event.data == b"donate":
-        await client.send_message(entity=event.chat_id, message="حمایت از ما")
-
-
-@client.on(events.CallbackQuery)
-async def temperature(event):
-    markup = client.build_reply_markup([Button.inline(text="بازگشت", data="back")])
-    if event.data == b"back":
-        await client.delete_messages(entity=event.chat_id,message_ids=event.message_id)
-        await start(event) 
+async def city(event: Message):
     if event.data == b"Tehran":
+        markup = [
+            Button.inline(text="تهران", data="tehran"),
+            Button.inline(text="کرج", data="karaj"),
+            Button.inline(text="شهریار", data="Shahriar"),
+        ]
         await client.edit_message(
-            entity=event.chat_id, 
+            entity=event.chat_id,
             message=event.message_id,
-            text=tehran,
-            parse_mode="html",
+            text=":شهر خود را انتخاب کنید",
             buttons=markup,
         )
-    if event.data == b"Karaj":
+    if event.data == b"KohgiluyehandBoyerAhmad":
+        markup = [
+            Button.inline(text="یاسوج", data="yasuj"),
+            Button.inline(text="دهدشت", data="dehdasht"),
+            Button.inline(text="گچسران", data="gachsaran"),
+        ]
         await client.edit_message(
-            entity=event.chat_id, 
+            entity=event.chat_id,
             message=event.message_id,
-            text=karaj,
-            parse_mode="html",
+            text=":شهر خود را انتخاب کنید",
             buttons=markup,
         )
-    if event.data == b"Esfahan":
+    if event.data == b"EastAzerbaijan":
+        markup = [
+            Button.inline(text="تبریز", data="tabriz"),
+            Button.inline(text="مراغه", data="maragheh"),
+            Button.inline(text="مرند", data="marand"),
+        ]
         await client.edit_message(
-            entity=event.chat_id, 
+            entity=event.chat_id,
             message=event.message_id,
-            text=esfahan,
-            parse_mode="html",
+            text=":شهر خود را انتخاب کنید",
             buttons=markup,
         )
-    if event.data == b"Shiraz":
+    if event.data == b"WestAzerbaijan":
+        markup = [
+            Button.inline(text="ارومیه", data="urmia"),
+            Button.inline(text="خوی", data="khoy"),
+            Button.inline(text="مهاباد", data="mahabad"),
+        ]
         await client.edit_message(
-            entity=event.chat_id, 
+            entity=event.chat_id,
             message=event.message_id,
-            text=shiraz,
-            parse_mode="html",
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Ardabil":
+        markup = [
+            Button.inline(text="اردبیل", data="ardabil"),
+            Button.inline(text="پارس آباد", data="parsabad"),
+            Button.inline(text="خلخال", data="khalkhal"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Isfahan":
+        markup = [
+            Button.inline(text="اصفهان", data="isfahan"),
+            Button.inline(text="کاشان", data="kashan"),
+            Button.inline(text="نجف آباد", data="najafabad"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Alborz":
+        markup = [
+            Button.inline(text="کرج", data="karaj"),
+            Button.inline(text="ساوجبلاغ", data="savojbolagh"),
+            Button.inline(text="نظرآباد", data="nazarabad"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Ilam":
+        markup = [
+            Button.inline(text="ایلام", data="ilam"),
+            Button.inline(text="دهلران", data="dehloran"),
+            Button.inline(text="ایوان", data="ivan"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Bushehr":
+        markup = [
+            Button.inline(text="بوشهر", data="bushehr"),
+            Button.inline(text="گناوه", data="ganaveh"),
+            Button.inline(text="دشتستان", data="dashtestan"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"ChaharmahalandBakhtiari":
+        markup = [
+            Button.inline(text="شهرکرد", data="shahrekord"),
+            Button.inline(text="بروجن", data="borujen"),
+            Button.inline(text="لردگان", data="lordegan"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"SouthKhorasan":
+        markup = [
+            Button.inline(text="بیرجند", data="birjand"),
+            Button.inline(text="قائنات", data="ghainat"),
+            Button.inline(text="طبس", data="tabas"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"RazaviKhorasan":
+        markup = [
+            Button.inline(text="مشهد", data="mashhad"),
+            Button.inline(text="نیشابور", data="neyshabur"),
+            Button.inline(text="تربت حیدریه ", data="torbateheydarieh"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"NorthKhorasan":
+        markup = [
+            Button.inline(text="بجنورد", data="bojnourd"),
+            Button.inline(text="شیروان", data="shirvan"),
+            Button.inline(text="اسفراین", data="esfarayen"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Khuzestan":
+        markup = [
+            Button.inline(text="اهواز", data="ahvaz"),
+            Button.inline(text="آبادان", data="abadan"),
+            Button.inline(text="خرمشهر", data="khorramshahr"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Zanjan":
+        markup = [
+            Button.inline(text="زنجان", data="zanjan"),
+            Button.inline(text="آبهر", data="abhar"),
+            Button.inline(text="خدابنده", data="khodabandeh"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"SistanandBaluchestan":
+        markup = [
+            Button.inline(text="زاهدان", data="Zahedan"),
+            Button.inline(text="چابهار", data="Chabahar"),
+            Button.inline(text="زابل", data="Zabol"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Qazvin":
+        markup = [
+            Button.inline(text="قزوین", data="qazvin"),
+            Button.inline(text="تاکستان", data="takestan"),
+            Button.inline(text="آبیک", data="abiyek"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Qom":
+        markup = [
+            Button.inline(text="قم", data="qom"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Kurdistan":
+        markup = [
+            Button.inline(text="سنندج", data="sanandaj"),
+            Button.inline(text="سقز", data="saghez"),
+            Button.inline(text="بانه", data="baneh"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Kerman":
+        markup = [
+            Button.inline(text="کرمان", data="kerman"),
+            Button.inline(text="سیرجان", data="sirjan"),
+            Button.inline(text="جیرفت", data="jiroft"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Kermanshah":
+        markup = [
+            Button.inline(text="کرمانشاه", data="kermanshah"),
+            Button.inline(text="اسلام آباد", data="eslamabad-e gharb"),
+            Button.inline(text="سنقر", data="songhor"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Golestan":
+        markup = [
+            Button.inline(text="گرگان", data="gorgan"),
+            Button.inline(text="گنبد کاووس", data="gonbad-e kavus"),
+            Button.inline(text="بندرترکمان", data="bandar torkaman"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Gilan":
+        markup = [
+            Button.inline(text="رشت", data="rasht"),
+            Button.inline(text="لاهیجان", data="lahijan"),
+            Button.inline(text="بندرانزلی", data="anzali"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Lorestan":
+        markup = [
+            Button.inline(text="خرم آباد", data="khorramabad"),
+            Button.inline(text="بروجرد", data="borujerd"),
+            Button.inline(text="دورود", data="dorud"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Mazandaran":
+        markup = [
+            Button.inline(text="ساری", data="sari"),
+            Button.inline(text="بابل", data="babol"),
+            Button.inline(text="آمل", data="amol"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Markazi":
+        markup = [
+            Button.inline(text="اراک", data="arak"),
+            Button.inline(text="ساوه", data="saveh"),
+            Button.inline(text="خمین", data="khomein"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Hormozgan":
+        markup = [
+            Button.inline(text="بندعباس", data="bandar abbas"),
+            Button.inline(text="قشم", data="qeshm"),
+            Button.inline(text="میناب", data="minab"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Hamadan":
+        markup = [
+            Button.inline(text="همدان", data="hamedan"),
+            Button.inline(text="ملایر", data="malayer"),
+            Button.inline(text="نهاوند", data="nahavand"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
+            buttons=markup,
+        )
+    if event.data == b"Yazd":
+        markup = [
+            Button.inline(text="یزد", data="yazd"),
+            Button.inline(text="میبد", data="meybod"),
+            Button.inline(text="اردکان", data="ardakan"),
+        ]
+        await client.edit_message(
+            entity=event.chat_id,
+            message=event.message_id,
+            text=":شهر خود را انتخاب کنید",
             buttons=markup,
         )
 
