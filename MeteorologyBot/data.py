8996ae84b8334c033282d12c955b29761143dff3
reza drakhshan <github2reza@gmail.com>
2024-03-28 17:43:57 +0330

    set Mysql database

---
+++ MeteorologyBot/data.py
@@ -0,0 +1,94 @@
+state = []
+cities = [
+    "tabriz",
+    "maragheh",
+    "marand",  # آذربایجان شرقی
+    "urmia",
+    "khoy",
+    "mahabad",  # آذربایجان غربی
+    "ardabil",
+    "parsabad",
+    "khalkhal",  # اردبیل
+    "isfahan",
+    "kashan",
+    "najafabad",  # اصفهان
+    "karaj",
+    "savojbolagh",
+    "nazarabad",  # البرز
+    "ilam",
+    "dehloran",
+    "ivan",  # ایلام
+    "bushehr",
+    "ganaveh",
+    "dashtestan",  # بوشهر
+    "tehran",
+    "karaj",
+    "shahriar",  # تهران
+    "shahrekord",
+    "borujen",
+    "lordegan",  # چهارمحال و بختیاری
+    "birjand",
+    "ghainat",
+    "tabas",  # خراسان جنوبی
+    "mashhad",
+    "neyshabur",
+    "torbat-e heydarieh",  # خراسان رضوی
+    "bojnourd",
+    "shirvan",
+    "esfarayen",  # خراسان شمالی
+    "ahvaz",
+    "abadan",
+    "khorramshahr",  # خوزستان
+    "zanjan",
+    "abhar",
+    "khodabandeh",  # زنجان
+    "semnan",
+    "shahrood",
+    "damghan",  # سمنان
+    "zahedan",
+    "chabahar",
+    "zabol",  # سیستان و بلوچستان
+    "shiraz",
+    "jahrom",
+    "marvdasht",  # فارس
+    "qazvin",
+    "takestan",
+    "abiyek",  # قزوین
+    "qom",  # قم
+    "sanandaj",
+    "saghez",
+    "baneh",  # کردستان
+    "kerman",
+    "sirjan",
+    "jiroft",  # کرمان
+    "kermanshah",
+    "eslamabad-e gharb",
+    "songhor",  # کرمانشاه
+    "yasuj",
+    "dehdasht",
+    "gachsaran",  # کهگیلویه و بویراحمد
+    "gorgan",
+    "gonbad-e kavus",
+    "bandar torkaman",  # گلستان
+    "rasht",
+    "lahijan",
+    "anzali",  # گیلان
+    "khorramabad",
+    "borujerd",
+    "dorud",  # لرستان
+    "sari",
+    "babol",
+    "amol",  # مازندران
+    "arak",
+    "saveh",
+    "khomein",  # مرکزی
+    "bandar abbas",
+    "qeshm",
+    "minab",  # هرمزگان
+    "hamedan",
+    "malayer",
+    "nahavand",  # همدان
+    "yazd",
+    "meybod",
+    "ardakan",  # یزد
+]
